6dde765b5ce4098deefcc950a5a51657c4fb7060
chenhong <15168264355@163.com>
2019-01-09 13:05:39 +0800

    Initial commit

---
+++ app/src/main/java/com/gjf/wc/activity/MainActivity.java
@@ -0,0 +1,114 @@
+package com.gjf.wc.activity;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.gjf.wc.R;
+import com.gjf.wc.observable.WechatObserver;
+import com.gjf.wc.utils.WechatHelper;
+import com.gjf.wc.utils.WechatInfoSpHelper;
+
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
+    private TextView textView;
+
+    private WechatAuthObserver mWechatAuthObserver = new WechatAuthObserver();
+
+    private LoginHandler mHandler = new LoginHandler(this);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.bt_auth) {
+            auth();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        WechatHelper.getInstance().removeWechatObserver(mWechatAuthObserver);
+}
+
+    private void initView() {
+        button = findViewById(R.id.bt_auth);
+        textView = findViewById(R.id.tv_info);
+
+        button.setOnClickListener(this);
+    }
+
+    private void auth() {
+        textView.setText("");
+
+        WechatHelper.getInstance().addWechatObserver(mWechatAuthObserver);
+        WechatHelper.getInstance().authorizeByWechat();
+    }
+
+    private void wechatAuthError() {
+        textView.setText("微信校验失败");
+    }
+
+    private void wechatAuthSuccess() {
+        // 微信授权结束后回调到此方法中
+        textView.setText("微信昵称为 :" + WechatInfoSpHelper.getWechatUserNickname());
+    }
+
+    private class WechatAuthObserver extends WechatObserver {
+
+        @Override
+        public void handleStateChange(int data) {
+            mHandler.sendEmptyMessage(data);
+        }
+    }
+
+    static class LoginHandler extends Handler {
+
+        private final WeakReference<MainActivity> mActivityRef;
+
+        LoginHandler(MainActivity activity) {
+            mActivityRef = new WeakReference<>(activity);
+        }
+
+        // 子类必须重写此方法,接受数据
+        @Override
+        public void handleMessage(final Message msg) {
+            MainActivity activity = mActivityRef.get();
+
+            if (activity == null) {
+                return;
+            }
+
+            int code = msg.what;
+
+            switch (code) {
+                case WechatHelper.WECHAT_CHECK_SUCCESS:
+                    WechatHelper.getInstance().removeWechatObserver(activity.mWechatAuthObserver);
+                    activity.wechatAuthSuccess();
+                    break;
+                case WechatHelper.WECHAT_CHECK_FAILURE:
+                    WechatHelper.getInstance().removeWechatObserver(activity.mWechatAuthObserver);
+                    activity.wechatAuthError();
+                    break;
+                default:
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/gjf/wc/utils/UserConstants.java
@@ -0,0 +1,14 @@
+package com.gjf.wc.utils;
+
+/**
+ * Created by guojunfu on 18/3/15.
+ * <p>
+ * 微信注册信息
+ */
+
+public class UserConstants {
+    public static String WECHAT_APPIDS = "wxb094078bdb6cd3df";
+    public static String WECHAT_SECRET = "9cd7499b1ba2d052b566a4e2e4c46f47";
+
+
+}
